5e5ae2df90e407c866070bf8eaf39335fd13685b
jeffrey-arndt <103957992+jeffrey-arndt@users.noreply.github.com>
2025-09-08 09:05:32 -0400

    Add files via upload

--- apis.tf
+++ apis.tf
@@ -11,16 +11,18 @@ resource "konnect_api" "my_api" {
 resource "konnect_api_implementation" "my_apiimplementation" {
   provider = konnect-beta
   api_id   = konnect_api.my_api.id
-  service = {
-    control_plane_id = var.control_plane_id
-    id               = konnect_gateway_service.flights_service.id
+  service_reference = {
+    service = {
+      control_plane_id = var.control_plane_id
+      id               = konnect_gateway_service.flights_service.id
+    }
   }
 }
 
 resource "konnect_api_publication" "my_apipublication" {
   provider                   = konnect-beta
   api_id                     = konnect_api.my_api.id
-  portal_id                  = "8dce862d-6d59-4095-a41c-d6cb8fa5deeb"
+  portal_id                  = var.portal_id
   auto_approve_registrations = true
   visibility                 = "public"
   auth_strategy_ids = [
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 #Variables
 variable "control_plane_id" {
   type = string
-  default = "740027f9-465b-4670-abe2-a80811a71f16" # Enter CP ID provided by Platform Team here
+  default = "" # Enter CP ID provided by Platform Team here
 }
 
 variable "portal_id" {
     type = string
-    default = "8dce862d-6d59-4095-a41c-d6cb8fa5deeb" # Enter Portal ID provided by the Platform team
+    default = "" # Enter Portal ID provided by the Platform team
 }
